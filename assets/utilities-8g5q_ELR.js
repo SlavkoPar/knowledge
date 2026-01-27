96b263e75ea2320d3a192240c0cf8d1f5fdf9563
SlavkoPar <slavko.parezanin@gmail.com>
2026-01-27 09:50:12 +0100

    Updates

---
+++ assets/utilities-8g5q_ELR.js
@@ -0,0 +1,2 @@
+const c=e=>e?new Date(e).toLocaleDateString()+" "+new Date(e).toLocaleTimeString():"",n=e=>e?new Date(e).toLocaleString([],{dateStyle:"short",timeStyle:"short"}):"",l=e=>e.replace(/[.*+?^${}()|[\]\\]/g,"\\$&");function s(e,a=20){let t;return(...o)=>{clearTimeout(t),t=setTimeout(()=>e(...o),a)}}export{n as a,s as d,l as e,c as f};
+//# sourceMappingURL=utilities-8g5q_ELR.js.map
