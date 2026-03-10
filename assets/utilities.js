10a8ca37e22273398b829a953e5772747d75e202
SlavkoPar <slavko.parezanin@gmail.com>
2026-03-10 15:24:45 +0100

    Updates

--- assets/utilities.js
+++
@@ -1,2 +0,0 @@
-const c=e=>e?new Date(e).toLocaleDateString()+" "+new Date(e).toLocaleTimeString():"",n=e=>e?new Date(e).toLocaleString([],{dateStyle:"short",timeStyle:"short"}):"",l=e=>e.replace(/[.*+?^${}()|[\]\\]/g,"\\$&");function s(e,a=20){let t;return(...o)=>{clearTimeout(t),t=setTimeout(()=>e(...o),a)}}export{n as a,s as d,l as e,c as f};
-//# sourceMappingURL=utilities.js.map
